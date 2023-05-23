1e99f2eb02b2261c1a2d49c4e50b4cfefeb4a936
Nicolás Londoño <nicolas.londono@veevart.com>
2023-05-23 17:42:20 -0500

    Add: createUser endpoint

--- Node/controllers/auth.js
+++ Node/controllers/auth.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const Usuario = require("../models/Usuario");
+const bcrypt = require("bcryptjs");
 
-const createUser = (req, res = express.request) => {
+const createUser = async (req, res = express.request) => {
     const { name, email, password } = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({email:email});
+        if(usuario){
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario ya existe"
+            })
+        }
+        usuario = new Usuario({name,email,password});
+        const salt = bcrypt.genSaltSync();
+        usuario.password= bcrypt.hashSync(password,salt);
+        await usuario.save();
+        res.status(200).json({
+            ok:true,
+            usuario
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            error
+        })
+    }
     res.status(200).json({
         ok: true,
         name, email, password
--- Node/middlewares/validarCampos.js
+++ Node/middlewares/validarCampos.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { validationResult } = require("express-validator")
 
-const validarCampos = (req, res = express.request) => {
+const validarCampos = (req, res = express.request, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({
